5519b1caf2acefd363ec82201703904aed496d58
Leahnicoles <lnsimonn@hotmail.com>
2023-06-13 00:03:04 +0100

    refactor code

--- game.py
+++ game.py
@@ -3,15 +3,18 @@ import random
 
 # Create a function to create a random number between 1 and 10
 def guess_number():
-    number = random.randint(1, 10)
+    return random.randint(1, 10)
 
 #Players Details
 player_name = input("Hello, What's your name?")
 
-#No. Guesses Variable
+# Number of guesses variable
 number_of_guesses = 0
 
 print(f'okay! {player_name} I am Guessing a number between 1 and 10:')
+
+# Get the random number
+number = guess_number()
 
 #While Loop
 while number_of_guesses < 5:
@@ -19,11 +22,11 @@ while number_of_guesses < 5:
     number_of_guesses += 1
     if guess < number:
         print('Your guess is too low')
-    if guess > number:
+    elif guess > number:
         print('Your guess is too high')
-    if guess == number:
+    else:
+        print(f"Congratulations, {player_name}! You guessed the number in {number_of_guesses} guesses!")
         break
-if guess == number:
-    print('You guessed the number in ' + str(number_of_guesses) + ' guesses!')
-else:
-    print('You did not guess the number, The number was ' + str(number))
+        
+if number_of_guesses == 5 and guess != number:
+    print(f"You did not guess the number, {player_name}. The number was {number}.")
